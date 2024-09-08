ed7db62b09e596ab1c5ab48e4efae9848ba74cf0
haquocdat543 <wwwdatha543@gmail.com>
2024-09-08 14:58:47 +0700

    feat(nvim-tree): neovim - auto close

--- init.lua
+++ init.lua
@@ -459,7 +459,7 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require("nvim-tree").setup({
-   -- Automatically update the tree when changing directories
+  -- Automatically update the tree when changing directories
   sync_root_with_cwd = true,
   respect_buf_cwd = true,
   update_cwd = true,
@@ -481,6 +481,15 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = false,
   },
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
 })
 
 local function my_on_attach(bufnr)
