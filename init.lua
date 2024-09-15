0c1cab4665bb90131fcd65531369eefd18bd4d02
haquocdat543 <wwwdatha543@gmail.com>
2024-09-15 18:59:29 +0700

    feat(lsp): prettier format - save before and after format

--- init.lua
+++ init.lua
@@ -318,11 +318,13 @@ vim.api.nvim_set_keymap('n', '<leader>p', '', {
 	noremap = true,
 	silent = true,
 	callback = function()
+		vim.cmd('write')
 		vim.lsp.buf.format({
 			async = true,
 			cmd = prettier.formatCommand,
 			env = prettier.env,
 		})
+		vim.cmd('write')
 	end
 })
 
