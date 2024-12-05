2932aa5f91adb508da70ef152be7d5c55bb27ebd
haquocdat543 <wwwdatha543@gmail.com>
2024-12-05 12:27:34 +0700

    plugin/key: add smear cursor neovim

--- init.lua
+++ init.lua
@@ -46,7 +46,7 @@ vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search(
 })
 
 -- Dadbod UI
-keymap.set('n', 'dt', ':DBUIToggle')
+keymap.set('n', '<leader>dt', ':DBUIToggle')
 
 -- Query { Rest API ]
 keymap.set('n', ',xr', ':call VrcQuery()<CR>')
@@ -195,8 +195,29 @@ keymap.set('n', '<leader>db', ':Dashboard<CR>')
 keymap.set('n', '<leader>tl', ':Telescope<CR>')
 
 local plugins = {
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			-- Smear cursor color. Defaults to Cursor GUI color if not set.
+			-- Set to "none" to match the text color at the target cursor position.
+			cursor_color = "#d3cdc3",
+
+			-- Background color. Defaults to Normal GUI background color if not set.
+			normal_bg = "#282828",
+
+			-- Smear cursor when switching buffers or windows.
+			smear_between_buffers = true,
+
+			-- Smear cursor when moving within line or to neighbor lines.
+			smear_between_neighbor_lines = true,
+
+			-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+			-- Smears will blend better on all backgrounds.
+			legacy_computing_symbols_support = false,
+		},
+	},
 	"tpope/vim-dadbod",
-        "kristijanhusak/vim-dadbod-ui",
+	"kristijanhusak/vim-dadbod-ui",
 	"Ramilito/kubectl.nvim",
 	{
 		{
