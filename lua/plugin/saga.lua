f5da3e25c964f5fbe7546ddf20b6fea09ce36438
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 16:33:19 +0700

    plugin(saga): move on attach

--- lua/plugin/saga.lua
+++ lua/plugin/saga.lua
@@ -17,3 +17,41 @@ saga.setup({
 		},
 	},
 })
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap
+local on_attach = function(client, bufnr)
+	opts.buffer = bufnr
+	keymap.set("n", "gs", "<cmd>Lspsaga show_workspace_diagnostics<CR>", opts)
+	keymap.set("n", "ca", "<cmd>Lspsaga code_action<CR>", opts)
+	keymap.set("n", "gn", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+	keymap.set("n", "gp", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+	keymap.set("n", "gP", "<Cmd>Lspsaga peek_definition<CR>", opts)
+	keymap.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts)
+	opts.desc = "Show LSP references"
+	keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+	opts.desc = "Go to declaration"
+	keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	opts.desc = "Show LSP definitions"
+	keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+	opts.desc = "Show LSP implementations"
+	keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+	opts.desc = "Show LSP type definitions"
+	keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+	opts.desc = "See available code actions"
+	keymap.set({ "n", "v" }, ",ca", vim.lsp.buf.code_action, opts)
+	opts.desc = "Smart rename"
+	keymap.set("n", ",rn", vim.lsp.buf.rename, opts)
+	opts.desc = "Show buffer diagnostics"
+	keymap.set("n", ",D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+	opts.desc = "Show line diagnostics"
+	keymap.set("n", ",d", vim.diagnostic.open_float, opts)
+	opts.desc = "Go to previous diagnostic"
+	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+	opts.desc = "Go to next diagnostic"
+	keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	opts.desc = "Show documentation for what is under cursor"
+	keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	opts.desc = "Restart LSP"
+	keymap.set("n", ",rs", ":LspRestart<CR>", opts)
+end
