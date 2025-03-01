376c2b62de497d585f202caaab0e5209e87ed89f
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 17:33:49 +0700

    plugin(telescope): add picker config for live grep

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -4,3 +4,13 @@ local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
 	return
 end
+
+telescope.setup {
+	pickers = {
+		live_grep = {
+			additional_args = function(opts)
+				return { "--hidden" }
+			end
+		},
+	},
+}
