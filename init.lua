662342831164f2fa49931bf3c13d49741d45e159
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 16:35:45 +0700

    init: remove unused things

--- init.lua
+++ init.lua
@@ -334,9 +334,6 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
--- require("nvim-tree").setup()
-
 -- OR setup with some options
 local function tab_win_closed(winnr)
 	local api = require "nvim-tree.api"
@@ -374,30 +371,6 @@ vim.api.nvim_create_autocmd("WinClosed", {
 	nested = true
 })
 
-local function my_on_attach(bufnr)
-	local api = require "nvim-tree.api"
-
-	local function opts(desc)
-		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	end
-
-	-- default mappings
-	api.config.mappings.default_on_attach(bufnr)
-
-	-- custom mappings
-	vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-	vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-end
-
--- pass to setup along with your other options
--- require("nvim-tree").setup {
--- ---
--- on_attach = my_on_attach,
--- ---
--- }
-
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
 require("core.colorscheme")
 require("core.keymap")
 require("core.format")
