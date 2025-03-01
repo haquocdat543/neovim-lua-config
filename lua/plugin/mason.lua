a67e595d3c5c85aab909f8e62cf596da8d7f5291
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 22:17:30 +0700

    plugin(mason): remove [ensure_installed] config

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -1,10 +1,6 @@
 -- import mason
 local mason = require("mason")
 
-local mason_lspconfig = require("mason-lspconfig")
-
-local mason_tool_installer = require("mason-tool-installer")
-
 -- enable mason and configure icons
 mason.setup({
 	ui = {
@@ -14,21 +10,7 @@ mason.setup({
 			package_uninstalled = "✗",
 		},
 	},
-	ensure_installed = {
-		"ts_ls",
-		"html",
-		"cssls",
-		"tailwindcss",
-		"svelte",
-		"lua_ls",
-		"graphql",
-		"emmet_ls",
-		"prismals",
-		"pyright",
-		"rust_analyzer",
-		"clangd",
-		"gopls",
-	},
+	ensure_installed = {},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
 })
