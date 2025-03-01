98ae4f806f16ccb6afa1043ee6732a581963e29a
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 23:52:10 +0700

    plugin(lsp): add dockerls

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,4 +1,15 @@
--- terraform lsp
+require("lspconfig").dockerls.setup({
+	settings = {
+		docker = {
+			languageserver = {
+				formatter = {
+					ignoreMultilineInstructions = true,
+				},
+			},
+		},
+	},
+})
+
 local signs = { Error = "e ", Warn = "w ", Hint = "h ", Info = "i " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
