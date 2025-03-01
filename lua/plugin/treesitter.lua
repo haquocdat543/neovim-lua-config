d8e6d9574b707681aca3119192ad84017d60c2d8
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 22:59:20 +0700

    plugin(treesitter): remove first config and format

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,19 +1,4 @@
-require("nvim-treesitter.configs").setup {
-	highlight = {
-		-- ...
-	},
-	-- ...
-	rainbow = {
-		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
-	}
-}
-
-require('nvim-treesitter.configs').setup {
+require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
 	-- ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
 
@@ -25,14 +10,14 @@ require('nvim-treesitter.configs').setup {
 	ensure_installed = { "javascript", "c", "lua", "vim", "vimdoc", "helm", "dockerfile", "yaml", "gotmpl" },
 	-- open_on_setup = true,
 	highlight = { enable = true },
-	indent = { enable = true, disable = { 'python' } },
+	indent = { enable = true, disable = { "python" } },
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = '<c-f>',
-			node_incremental = '<c-f>',
-			scope_incremental = '<c-s>',
-			node_decremental = '<M-f>',
+			init_selection = "<c-f>",
+			node_incremental = "<c-f>",
+			scope_incremental = "<c-s>",
+			node_decremental = "<M-f>",
 		},
 	},
 	textobjects = {
@@ -41,42 +26,42 @@ require('nvim-treesitter.configs').setup {
 			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 			keymaps = {
 				-- You can use the capture groups defined in textobjects.scm
-				['aa'] = '@parameter.outer',
-				['ia'] = '@parameter.inner',
-				['af'] = '@function.outer',
-				['if'] = '@function.inner',
-				['ac'] = '@class.outer',
-				['ic'] = '@class.inner',
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
 			},
 		},
 		move = {
 			enable = true,
 			set_jumps = true, -- whether to set jumps in the jumplist
 			goto_next_start = {
-				[']m'] = '@function.outer',
-				[']]'] = '@class.outer',
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
 			},
 			goto_next_end = {
-				[']M'] = '@function.outer',
-				[']['] = '@class.outer',
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
 			},
 			goto_previous_start = {
-				['[m'] = '@function.outer',
-				['[['] = '@class.outer',
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
 			},
 			goto_previous_end = {
-				['[M'] = '@function.outer',
-				['[]'] = '@class.outer',
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
 			},
 		},
 		swap = {
 			enable = true,
 			swap_next = {
-				[',a'] = '@parameter.inner',
+				[",a"] = "@parameter.inner",
 			},
 			swap_previous = {
-				[',A'] = '@parameter.inner',
+				[",A"] = "@parameter.inner",
 			},
 		},
 	},
-}
+})
