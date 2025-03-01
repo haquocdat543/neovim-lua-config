80e3839d27225965850c80ce58562e50b902acc3
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 16:34:18 +0700

    plugin(lsp): move sign

--- init.lua
+++ init.lua
@@ -398,12 +398,6 @@ end
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 -- (not in youtube nvim video)
-local signs = { Error = "e ", Warn = "w ", Hint = "h ", Info = "i " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
 require("core.colorscheme")
 require("core.keymap")
 require("core.format")
--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -6,3 +6,9 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		vim.lsp.buf.format()
 	end,
 })
+
+local signs = { Error = "e ", Warn = "w ", Hint = "h ", Info = "i " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
