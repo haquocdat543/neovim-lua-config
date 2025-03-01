b4afa389ad6f956772617f11f55c3321a1a8411f
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 18:23:26 +0700

    core(format): remove config

--- lua/core/format.lua
+++ lua/core/format.lua
@@ -1,14 +1,1 @@
--- Key mapping with anonymous function to run Prettier formatting
-vim.api.nvim_set_keymap('n', '<leader>p', '', {
-	noremap = true,
-	silent = true,
-	callback = function()
-		vim.cmd('write')
-		vim.lsp.buf.format({
-			async = true,
-			cmd = prettier.formatCommand,
-			env = prettier.env,
-		})
-		vim.cmd('write')
-	end
-})
+
