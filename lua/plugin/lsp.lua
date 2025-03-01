f4a1c36da5c2b8595bbce1b100493358f56c0566
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 22:01:24 +0700

    plugin(lsp): remove config

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,12 +1,4 @@
 -- terraform lsp
-require 'lspconfig'.terraformls.setup {}
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.tf", "*.tfvars" },
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-})
-
 local signs = { Error = "e ", Warn = "w ", Hint = "h ", Info = "i " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
