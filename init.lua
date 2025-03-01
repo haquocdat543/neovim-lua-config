d2901d1823922102f25b308215f734106d9a4722
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 15:58:47 +0700

    lua(config): seperate lualine

--- init.lua
+++ init.lua
@@ -295,10 +295,6 @@ local plugins = {
 local opts = {}
 
 require("lazy").setup(plugins, opts)
-local status, lualine = pcall(require, "lualine")
-if not status then
-	return
-end
 
 -- shfmt configuration for Bash
 local shfmt = {
@@ -522,36 +518,6 @@ require('nvim-treesitter.configs').setup {
 	},
 }
 
--- get lualine nightfly theme
-local lualine_nightfly = require("lualine.themes.catppuccin")
-
--- new colors for theme
--- local new_colors = {
--- blue = "#65D1FF",
--- green = "#3EFFDC",
--- violet = "#FF61EF",
--- yellow = "#FFDA7B",
--- black = "#000000",
--- }
-
--- change nightlfy theme colors
--- lualine_nightfly.normal.a.bg = new_colors.blue
--- lualine_nightfly.insert.a.bg = new_colors.green
--- lualine_nightfly.visual.a.bg = new_colors.violet
--- lualine_nightfly.command = {
--- a = {
--- gui = "bold",
--- bg = new_colors.yellow,
--- fg = new_colors.black, -- black
--- },
--- }
-
--- configure lualine with modified theme
-lualine.setup({
-	options = {
-		theme = lualine_nightfly,
-	},
-})
 -- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
@@ -998,3 +964,4 @@ require("plugin.spectre")
 require("plugin.telescope")
 require("plugin.hop")
 require("plugin.tokyonight")
+require("plugin.lualine")
