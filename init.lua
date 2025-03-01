7b09f5b38c21a24a82b78146e9b56a5160354383
haquocdat543 <wwwdatha543@gmail.com>
2025-03-01 16:38:44 +0700

    init: remove unnecessary things

--- init.lua
+++ init.lua
@@ -296,16 +296,6 @@ local opts = {}
 
 require("lazy").setup(plugins, opts)
 
--- Ensure termguicolors is enabled if not already
-vim.opt.termguicolors = true
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
 require("core.colorscheme")
 require("core.keymap")
 require("core.format")
