0c22bdaaa6ce927839e7785e78265250231931ed
haquocdat543 <wwwdatha543@gmail.com>
2025-03-02 16:45:04 +0700

    plugin(mason): update lsp list

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -4,7 +4,14 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason.setup()
 mason_lspconfig.setup({
-	ensure_installed = { "lua_ls", "tsserver", "dockerls", "yamlls" },
+	ensure_installed = {
+		"lua_ls",
+		"tsserver",
+		"dockerls",
+		"yamlls",
+		"jdtls",
+		"jsonls",
+	},
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -13,7 +20,14 @@ local on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 end
 
-local servers = { "lua_ls", "ts_ls", "dockerls", "yamlls", "jdtls" }
+local servers = {
+	"lua_ls",
+	"ts_ls",
+	"dockerls",
+	"yamlls",
+	"jdtls",
+	"jsonls",
+}
 for _, server in ipairs(servers) do
 	lspconfig[server].setup({
 		on_attach = on_attach,
