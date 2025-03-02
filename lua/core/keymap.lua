a7cd85979ffb94245388358d1d1853eb571bfa3e
haquocdat543 <wwwdatha543@gmail.com>
2025-03-02 21:35:16 +0700

    core(keymap): re use vim

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,97 +1,96 @@
-local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 ----------------------------------------------------------------------------------------------------
 -- System
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<C-b>b", ":set backup<CR>")
-keymap.set("n", "<C-h>h", ":noh<CR>")
-keymap.set("n", "<C-q>q", ":q!<CR>")
-keymap.set("n", "<C-q>w", ":wq!<CR>")
+vim.keymap.set("n", "<C-b>b", ":set backup<CR>")
+vim.keymap.set("n", "<C-h>h", ":noh<CR>")
+vim.keymap.set("n", "<C-q>q", ":q!<CR>")
+vim.keymap.set("n", "<C-q>w", ":wq!<CR>")
 
 -- System - replacement & navigation
-keymap.set("n", "<C-c>1", ":%s///g<Left><Left><Left>")
-keymap.set("n", "<C-c>2", ":.,.s///g<Left><Left><Left>")
-keymap.set("n", "<C-c>3", ":.,0s///g<Left><Left><Left>")
-keymap.set("n", "<C-c>4", ":.,$s///g<Left><Left><Left>")
-keymap.set("n", "<C-c>5", ":.,.-s///g<Left><Left><Left><Left><Left>")
-keymap.set("n", "<C-c>6", ":.,.+s///g<Left><Left><Left><Left><Left>")
-keymap.set("n", "<C-x>", "<C-p><CR>")
-keymap.set("n", "<A-j>", ":m .+1<CR>==")
-keymap.set("n", "<A-k>", ":m .-2<CR>==")
-keymap.set("n", "<leader>aa", ":UltiSnipsEdit<CR>")
-keymap.set("n", "<leader>ww", ":w!<CR>")
-keymap.set("n", "<C-s>s", ":<ESC>w!<CR>")
-keymap.set("n", "<A-n>n", ":n<CR>")
-keymap.set("n", "<A-p>p", ":p<CR>")
+vim.keymap.set("n", "<C-c>1", ":%s///g<Left><Left><Left>")
+vim.keymap.set("n", "<C-c>2", ":.,.s///g<Left><Left><Left>")
+vim.keymap.set("n", "<C-c>3", ":.,0s///g<Left><Left><Left>")
+vim.keymap.set("n", "<C-c>4", ":.,$s///g<Left><Left><Left>")
+vim.keymap.set("n", "<C-c>5", ":.,.-s///g<Left><Left><Left><Left><Left>")
+vim.keymap.set("n", "<C-c>6", ":.,.+s///g<Left><Left><Left><Left><Left>")
+vim.keymap.set("n", "<C-x>", "<C-p><CR>")
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<leader>aa", ":UltiSnipsEdit<CR>")
+vim.keymap.set("n", "<leader>ww", ":w!<CR>")
+vim.keymap.set("n", "<C-s>s", ":<ESC>w!<CR>")
+vim.keymap.set("n", "<A-n>n", ":n<CR>")
+vim.keymap.set("n", "<A-p>p", ":p<CR>")
 
 -- System
-keymap.set("n", "<leader>pa", '"*p')
-keymap.set("n", "<leader>ya", "gg\"*yG''")
-keymap.set("n", "<leader>yy", '"*yy')
-keymap.set("n", "<leader>el", "yypC")
-keymap.set("n", "<leader>eL", "yyPC")
+vim.keymap.set("n", "<leader>pa", '"*p')
+vim.keymap.set("n", "<leader>ya", "gg\"*yG''")
+vim.keymap.set("n", "<leader>yy", '"*yy')
+vim.keymap.set("n", "<leader>el", "yypC")
+vim.keymap.set("n", "<leader>eL", "yyPC")
 
 -- System
-keymap.set("n", ",ch", ":checkhealth ")
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-keymap.set("n", "<C-a>", "gg<M-v>G")
-keymap.set("n", "ZQ", ":q!<CR>")
-keymap.set("n", "ZZ", ":wq!<CR>")
-keymap.set("n", "<leader>hr", ":%!xxd<CR> :set filetype=xxd<CR>")
-keymap.set("n", "<leader>hw", ":%!xxd -r<CR> :set binary<CR> :set filetype=<CR>")
-keymap.set("n", "<leader>el", "yypC")
-keymap.set("n", "<leader>eL", "yyPC")
-keymap.set("n", "<leader>ra", ":call ReplaceAll")
+vim.keymap.set("n", ",ch", ":checkhealth ")
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+vim.keymap.set("n", "<C-a>", "gg<M-v>G")
+vim.keymap.set("n", "ZQ", ":q!<CR>")
+vim.keymap.set("n", "ZZ", ":wq!<CR>")
+vim.keymap.set("n", "<leader>hr", ":%!xxd<CR> :set filetype=xxd<CR>")
+vim.keymap.set("n", "<leader>hw", ":%!xxd -r<CR> :set binary<CR> :set filetype=<CR>")
+vim.keymap.set("n", "<leader>el", "yypC")
+vim.keymap.set("n", "<leader>eL", "yyPC")
+vim.keymap.set("n", "<leader>ra", ":call ReplaceAll")
 
 -- Map ESC
-keymap.set("i", "<C-x>", "<Esc><Esc>")
+vim.keymap.set("i", "<C-x>", "<Esc><Esc>")
 
 -- Nvimtree - resize
-keymap.set("n", "<leader><leader>v", ":vertical res ")
+vim.keymap.set("n", "<leader><leader>v", ":vertical res ")
 
 -- System - commands
-keymap.set("n", "<leader>rg", ":registers<CR>")
+vim.keymap.set("n", "<leader>rg", ":registers<CR>")
 
 -- Terminal
-keymap.set("n", "<leader>tn", ":split<CR><C-w>w:term<CR>i")
-keymap.set("n", "<leader>tm", ":split<CR><C-w>w:horizontal resize -10<CR>:term<CR>i")
+vim.keymap.set("n", "<leader>tn", ":split<CR><C-w>w:term<CR>i")
+vim.keymap.set("n", "<leader>tm", ":split<CR><C-w>w:horizontal resize -10<CR>:term<CR>i")
 
 -- Diff
-keymap.set("n", "<leader>dd", ":windo diffthis<CR>")
-keymap.set("n", "<leader>do", ":diffoff!<CR>")
+vim.keymap.set("n", "<leader>dd", ":windo diffthis<CR>")
+vim.keymap.set("n", "<leader>do", ":diffoff!<CR>")
 
 -- System - default file open
-keymap.set("n", "<leader>en", ":tabnew ~/.config/nvim/init.lua<CR>")
-keymap.set("n", "<leader>ev", ":tabnew ~/.vimrc<CR>")
-keymap.set("n", "<leader>ea", ":tabnew ~/.bashrc<CR>")
-keymap.set("n", "<leader>ec", ":tabnew ~/.ssh/config<CR>")
-keymap.set("n", "<leader>sc", ":tabnew ~/Suitcase/bash/scripts/BasicCalculator.sh<CR>")
-keymap.set("n", "<leader>sn", ":tabnew ~/Suitcase/snippets/sh.snippets<CR>")
-keymap.set("n", "<leader>nn", ":set nopaste<CR>")
-keymap.set("n", "<leader>nm", ":set paste<CR>")
+vim.keymap.set("n", "<leader>en", ":tabnew ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "<leader>ev", ":tabnew ~/.vimrc<CR>")
+vim.keymap.set("n", "<leader>ea", ":tabnew ~/.bashrc<CR>")
+vim.keymap.set("n", "<leader>ec", ":tabnew ~/.ssh/config<CR>")
+vim.keymap.set("n", "<leader>sc", ":tabnew ~/Suitcase/bash/scripts/BasicCalculator.sh<CR>")
+vim.keymap.set("n", "<leader>sn", ":tabnew ~/Suitcase/snippets/sh.snippets<CR>")
+vim.keymap.set("n", "<leader>nn", ":set nopaste<CR>")
+vim.keymap.set("n", "<leader>nm", ":set paste<CR>")
 
 -- System - navigation
-keymap.set("n", "ss", ":split<CR>C-w>w")
-keymap.set("n", "sv", ":vsplit<CR><C-w>w")
-keymap.set("n", "sx", ":close<CR>")
-keymap.set("n", "sw", ":wincmd w<CR>")
-keymap.set("n", "sl", ":wincmd l<CR>")
-keymap.set("n", "sh", ":wincmd h<CR>")
-keymap.set("n", "sj", ":wincmd j<CR>")
-keymap.set("n", "sk", ":wincmd k<CR>")
+vim.keymap.set("n", "ss", ":split<CR>C-w>w")
+vim.keymap.set("n", "sv", ":vsplit<CR><C-w>w")
+vim.keymap.set("n", "sx", ":close<CR>")
+vim.keymap.set("n", "sw", ":wincmd w<CR>")
+vim.keymap.set("n", "sl", ":wincmd l<CR>")
+vim.keymap.set("n", "sh", ":wincmd h<CR>")
+vim.keymap.set("n", "sj", ":wincmd j<CR>")
+vim.keymap.set("n", "sk", ":wincmd k<CR>")
 
 -- System - tab navigation
-keymap.set("n", "<leader>te", ":tabedit ")
-keymap.set("n", "<leader>to", ":tabnew ")
-keymap.set("n", "<leader>tx", ":tabclose<CR>")
-keymap.set("n", "<leader>tn", ":tabn<CR>")
-keymap.set("n", "<leader>tp", ":tabp<CR>")
-keymap.set("n", "<leader>ev", ":tabnew $MYVIMRC<CR>")
-keymap.set("n", "<leader>ea", ":tabnew $HOME/.bashrc<CR>")
-keymap.set("n", "<leader>cr", ":tabnew $HOME/.aws/credentials<CR>")
-keymap.set("n", "<leader>cf", ":tabnew $HOME/.aws/config<CR>")
+vim.keymap.set("n", "<leader>te", ":tabedit ")
+vim.keymap.set("n", "<leader>to", ":tabnew ")
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>")
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>")
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>")
+vim.keymap.set("n", "<leader>ev", ":tabnew $MYVIMRC<CR>")
+vim.keymap.set("n", "<leader>ea", ":tabnew $HOME/.bashrc<CR>")
+vim.keymap.set("n", "<leader>cr", ":tabnew $HOME/.aws/credentials<CR>")
+vim.keymap.set("n", "<leader>cf", ":tabnew $HOME/.aws/config<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Pomodoro
@@ -126,106 +125,106 @@ vim.keymap.set(
 ----------------------------------------------------------------------------------------------------
 -- Ranger
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>rr", ":Ranger<CR>")
+vim.keymap.set("n", "<leader>rr", ":Ranger<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Snippet
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>sg", ":Snippets<CR>") -- sg = Suggest
+vim.keymap.set("n", "<leader>sg", ":Snippets<CR>") -- sg = Suggest
 
 ----------------------------------------------------------------------------------------------------
 -- Rest
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", ",xr", ":call VrcQuery()<CR>")
+vim.keymap.set("n", ",xr", ":call VrcQuery()<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Markdown
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>md", ":MarkdownPreview<CR>")
+vim.keymap.set("n", "<leader>md", ":MarkdownPreview<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Kubernetes
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>kk", '<cmd>lua require("kubectl").toggle()<CR>')
+vim.keymap.set("n", "<leader>kk", '<cmd>lua require("kubectl").toggle()<CR>')
 
 ----------------------------------------------------------------------------------------------------
 -- Packer
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>ii", ":PackerInstall<CR>")
-keymap.set("n", "<leader>is", ":PackerSync<CR>")
+vim.keymap.set("n", "<leader>ii", ":PackerInstall<CR>")
+vim.keymap.set("n", "<leader>is", ":PackerSync<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Terminal
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>ft", ":FloatermNew<CR>")
-keymap.set("n", "<leader>tt", ":terminal<CR>")
+vim.keymap.set("n", "<leader>ft", ":FloatermNew<CR>")
+vim.keymap.set("n", "<leader>tt", ":terminal<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Hop ( movement )
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader><leader>g", ":HopAnywhere<CR>")
-keymap.set("n", "<leader><leader>h", ":HopWord<CR>")
-keymap.set("n", "<leader><leader>l", ":HopLine<CR>")
-keymap.set("n", "<leader><leader>c", ":HopChar1<CR>")
-keymap.set("n", "<leader>b", ",,")
+vim.keymap.set("n", "<leader><leader>g", ":HopAnywhere<CR>")
+vim.keymap.set("n", "<leader><leader>h", ":HopWord<CR>")
+vim.keymap.set("n", "<leader><leader>l", ":HopLine<CR>")
+vim.keymap.set("n", "<leader><leader>c", ":HopChar1<CR>")
+vim.keymap.set("n", "<leader>b", ",,")
 
 ----------------------------------------------------------------------------------------------------
 -- Lazygit
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>ll", ":Lazy<CR>")
-keymap.set("n", "<leader>lg", ":LazyGit<CR>")
+vim.keymap.set("n", "<leader>ll", ":Lazy<CR>")
+vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Mason ( LSP )
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>mm", ":Mason<CR>")
-keymap.set("n", "<leader>mi", ":MasonInstall ")
+vim.keymap.set("n", "<leader>mm", ":Mason<CR>")
+vim.keymap.set("n", "<leader>mi", ":MasonInstall ")
 
 ----------------------------------------------------------------------------------------------------
 -- Fugitive git
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>gs", ":G status<CR>")
-keymap.set("n", "<leader>ga", ":G add ")
-keymap.set("n", "<leader>gA", ":G add .<CR>")
-keymap.set("n", "<leader>gc", ":G commit<CR>")
-keymap.set("n", "<leader>Gc", ":G add .<CR>:G commit<CR>")
-keymap.set("n", "<leader>gC", ":G checkout ")
-keymap.set("n", "<leader>gb", ":G branch<CR>")
-keymap.set("n", "<leader>gB", ":G branch ")
-keymap.set("n", "<leader>gm", ":G merge ")
-keymap.set("n", "<leader>gp", ":G pull<CR>")
-keymap.set("n", "<leader>gP", ":G push<CR>")
-keymap.set("n", "<leader>gl", ":G log<CR>")
+vim.keymap.set("n", "<leader>gs", ":G status<CR>")
+vim.keymap.set("n", "<leader>ga", ":G add ")
+vim.keymap.set("n", "<leader>gA", ":G add .<CR>")
+vim.keymap.set("n", "<leader>gc", ":G commit<CR>")
+vim.keymap.set("n", "<leader>Gc", ":G add .<CR>:G commit<CR>")
+vim.keymap.set("n", "<leader>gC", ":G checkout ")
+vim.keymap.set("n", "<leader>gb", ":G branch<CR>")
+vim.keymap.set("n", "<leader>gB", ":G branch ")
+vim.keymap.set("n", "<leader>gm", ":G merge ")
+vim.keymap.set("n", "<leader>gp", ":G pull<CR>")
+vim.keymap.set("n", "<leader>gP", ":G push<CR>")
+vim.keymap.set("n", "<leader>gl", ":G log<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Neogit
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>ng", ":Neogit<CR>")
+vim.keymap.set("n", "<leader>ng", ":Neogit<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Git fugitive
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>gb", ":Git blame<CR>")
+vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Nvim tree
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>")
-keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>")
-keymap.set("n", "<leader>nc", ":NvimTreeFocus<CR>")
--- keymap.set('n','<leader>nt',':NERDTreeToggle<CR>')
-keymap.set("n", "<leader>db", ":Dashboard<CR>")
+vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>")
+vim.keymap.set("n", "<leader>nc", ":NvimTreeFocus<CR>")
+-- vim.keymap.set('n','<leader>nt',':NERDTreeToggle<CR>')
+vim.keymap.set("n", "<leader>db", ":Dashboard<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- Telescope
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>tl", ":set nopaste<CR>:Telescope<CR>")
-keymap.set("n", "<leader>1", ":set nopaste<CR><Cmd>Telescope find_files hidden=true no_ignore=true<CR>")
-keymap.set("n", "<leader>2", ":set nopaste<CR><Cmd>Telescope live_grep hidden=true no_ignore=true<CR>")
-keymap.set("n", "<leader>3", ":set nopaste<CR><Cmd>Telescope buffers hidden=true no_ignore=true<CR>")
-keymap.set("n", "<leader>4", ":set nopaste<CR><Cmd>Telescope oldfiles hidden=true no_ignore=true<CR>")
-keymap.set("n", "<leader>5", ":Telescope projects<CR>")
-keymap.set("n", "<leader>6", ":Telescope telescope-tabs list_tabs<CR>")
+vim.keymap.set("n", "<leader>tl", ":set nopaste<CR>:Telescope<CR>")
+vim.keymap.set("n", "<leader>1", ":set nopaste<CR><Cmd>Telescope find_files hidden=true no_ignore=true<CR>")
+vim.keymap.set("n", "<leader>2", ":set nopaste<CR><Cmd>Telescope live_grep hidden=true no_ignore=true<CR>")
+vim.keymap.set("n", "<leader>3", ":set nopaste<CR><Cmd>Telescope buffers hidden=true no_ignore=true<CR>")
+vim.keymap.set("n", "<leader>4", ":set nopaste<CR><Cmd>Telescope oldfiles hidden=true no_ignore=true<CR>")
+vim.keymap.set("n", "<leader>5", ":Telescope projects<CR>")
+vim.keymap.set("n", "<leader>6", ":Telescope telescope-tabs list_tabs<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- LSP
@@ -276,7 +275,7 @@ end, { desc = "Show LSP Capabilities" })
 ----------------------------------------------------------------------------------------------------
 -- Format
 ----------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>fm", ":Format<CR>")
+vim.keymap.set("n", "<leader>fm", ":Format<CR>")
 
 ----------------------------------------------------------------------------------------------------
 -- DAP
