7fd875e8033492361b18ae2e854daeb312dde35f
haquocdat543 <wwwdatha543@gmail.com>
2025-03-02 14:58:05 +0700

    plugin(telescope): add keybinding on c-j c-k q

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,16 +1,31 @@
-require('telescope').load_extension('projects')
+require("telescope").load_extension("projects")
 
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
 	return
 end
 
-telescope.setup {
+local actions = require("telescope.actions")
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next, -- Move down
+				["<C-k>"] = actions.move_selection_previous, -- Move up
+				["<C-n>"] = false, -- Disable default <C-n>
+				["<C-p>"] = false, -- Disable default <C-p>
+			},
+			n = {
+				["q"] = actions.close, -- Quit with 'q' in normal mode
+			},
+		},
+	},
 	pickers = {
 		live_grep = {
 			additional_args = function(opts)
 				return { "--hidden" }
-			end
+			end,
 		},
 	},
-}
+})
