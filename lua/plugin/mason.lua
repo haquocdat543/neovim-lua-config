a3c24892fb0777330b03a3606b491187841df9f1
haquocdat543 <wwwdatha543@gmail.com>
2025-03-02 16:45:59 +0700

    plugin(mason): merge lsp list

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -2,24 +2,6 @@ local lspconfig = require("lspconfig")
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
-mason.setup()
-mason_lspconfig.setup({
-	ensure_installed = {
-		"lua_ls",
-		"tsserver",
-		"dockerls",
-		"yamlls",
-		"jdtls",
-		"jsonls",
-	},
-})
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-local on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-end
-
 local servers = {
 	"lua_ls",
 	"ts_ls",
@@ -28,6 +10,18 @@ local servers = {
 	"jdtls",
 	"jsonls",
 }
+
+mason.setup()
+mason_lspconfig.setup({
+	ensure_installed = servers,
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local on_attach = function(client, bufnr)
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+end
+
 for _, server in ipairs(servers) do
 	lspconfig[server].setup({
 		on_attach = on_attach,
