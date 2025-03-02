916b1fdf643e1f588463e6c4390cc7fee844854f
haquocdat543 <wwwdatha543@gmail.com>
2025-03-02 16:57:14 +0700

    plugin(mason): bind capabilities on installed servers

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -2,7 +2,7 @@ local lspconfig = require("lspconfig")
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
-local servers = {
+local lsps = {
 	"lua_ls",
 	"ts_ls",
 	"dockerls",
@@ -14,7 +14,7 @@ local servers = {
 
 mason.setup()
 mason_lspconfig.setup({
-	ensure_installed = servers,
+	ensure_installed = lsps,
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -22,6 +22,8 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 end
+
+local servers = mason_lspconfig.get_installed_servers()
 
 for _, server in ipairs(servers) do
 	lspconfig[server].setup({
