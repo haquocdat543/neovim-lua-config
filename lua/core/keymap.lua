7266cf14082ec9ba386f03810f8bc4746f075791
haquocdat543 <wwwdatha543@gmail.com>
2025-03-09 22:36:01 +0700

    Revert "plugin: remove barbar"
    This reverts commit 08237280ebccd606ba8f6dc630297ac1b5894998.

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -308,3 +308,64 @@ vim.keymap.set("n", "<Leader>du", function() require("dapui").toggle() end)
 vim.keymap.set("n", "<Leader>du", function() require("dapui").toggle() end)
 vim.keymap.set("n", "<leader>fm", ":Format<CR>")
 
+----------------------------------------------------------------------------------------------------
+-- BarBar
+----------------------------------------------------------------------------------------------------
+-- Move to previous/next
+vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<A-l>", "<Cmd>BufferNext<CR>")
+
+-- Re-order to previous/next
+vim.keymap.set("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>")
+vim.keymap.set("n", "<A->>", "<Cmd>BufferMoveNext<CR>")
+vim.keymap.set("n", "<A-H", "<Cmd>BufferMovePrevious<CR>")
+vim.keymap.set("n", "<A-L", "<Cmd>BufferMoveNext<CR>")
+
+-- Goto buffer in position...
+vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
+vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
+vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
+vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
+vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
+vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
+vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
+vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
+vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
+vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>")
+
+-- Pin/unpin buffer
+vim.keymap.set("n", "<A-p>", "<Cmd>BufferPin<CR>")
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>")
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+vim.keymap.set("n", "<C-p>", "<Cmd>BufferPick<CR>")
+vim.keymap.set("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>")
+
+-- Sort automatically by...
+vim.keymap.set("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>")
+vim.keymap.set("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>")
+vim.keymap.set("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>")
+vim.keymap.set("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>")
+vim.keymap.set("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>")
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
