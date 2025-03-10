2c06d7d3300ebbd2846355197465ba328ca4f6d5
haquocdat543 <wwwdatha543@gmail.com>
2025-03-11 00:43:52 +0700

    plugin(orgmode): seperate

--- lua/plugin/lazy.lua
+++ lua/plugin/lazy.lua
@@ -16,25 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Import plugin configurations
 require("lazy").setup({
 	{
-		"nvim-orgmode/orgmode",
-		event = "VeryLazy",
-		ft = { "org" },
-		config = function()
-			-- Setup orgmode
-			require("orgmode").setup({
-				org_agenda_files = "~/orgfiles/**/*",
-				org_default_notes_file = "~/orgfiles/refile.org",
-			})
-
-			-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-			-- add ~org~ to ignore_install
-			-- require('nvim-treesitter.configs').setup({
-			-- sure_installed = 'all',
-			-- ignore_install = { 'org' },
-			-- })
-		end,
-	},
-	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
@@ -129,4 +110,5 @@ require("lazy").setup({
 	require("plugin.crates"),
 	require("plugin.pomodoro"),
 	require("plugin.rustacean"),
+	require("plugin.orgmode"),
 })
