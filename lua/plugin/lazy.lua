af9e038f95113021447a0b87ff8c3ddc2c94e097
haquocdat543 <wwwdatha543@gmail.com>
2025-03-11 00:45:22 +0700

    plugin(barbar): seperate

--- lua/plugin/lazy.lua
+++ lua/plugin/lazy.lua
@@ -40,23 +40,6 @@ require("lazy").setup({
 			"MunifTanjim/nui.nvim",
 		},
 	},
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"lewis6991/gitsigns.nvim",  -- OPTIONAL: for git status
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
 	{ "codota/tabnine-nvim", build = "./dl_binaries.sh" },
 	"brenoprata10/nvim-highlight-colors",
 	dependencies = { "nvim-lua/plenary.nvim" },
@@ -111,4 +94,5 @@ require("lazy").setup({
 	require("plugin.pomodoro"),
 	require("plugin.rustacean"),
 	require("plugin.orgmode"),
+	require("plugin.barbar"),
 })
