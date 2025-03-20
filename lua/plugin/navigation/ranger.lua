95acf0853bdb3f6aaca8d405f628bfe479fd3e98
haquocdat543 <wwwdatha543@gmail.com>
2025-03-20 10:05:09 +0700

    core(plugin/navigation): ranger - update to use nvim plugin

--- lua/plugin/navigation/ranger.lua
+++ lua/plugin/navigation/ranger.lua
@@ -1,3 +1,12 @@
 return {
-	"francoiscabrol/ranger.vim",
+	"kelly-lin/ranger.nvim",
+	config = function()
+		require("ranger-nvim").setup({ replace_netrw = true })
+		vim.api.nvim_set_keymap("n", "<leader>q", "", {
+			noremap = true,
+			callback = function()
+				require("ranger-nvim").open(true)
+			end,
+		})
+	end,
 }
