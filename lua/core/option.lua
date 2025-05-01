7976b9fa17a045680da27fb8166317edaf907647
haquocdat543 <wwwdatha543@gmail.com>
2025-05-01 16:33:40 +0700

    style(core/option|prettier): format

--- lua/core/option.lua
+++ lua/core/option.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " " -- for normal mode
+vim.g.mapleader = " "      -- for normal mode
 vim.g.maplocalleader = " " -- for local leader mappings
 vim.opt.title = true
 vim.opt.hlsearch = true
@@ -16,11 +16,11 @@ vim.opt.wildmenu = true
 vim.opt.laststatus = 2
 vim.opt.cursorline = true
 
-vim.opt.tabstop = 2      -- Visually represent a tab as 2 spaces
-vim.opt.shiftwidth = 2   -- Indent by 2 spaces
-vim.opt.softtabstop = 2  -- Backspace removes 2 spaces at a time
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.autoindent = true -- Maintain indentation level on new lines
+vim.opt.tabstop = 2        -- Visually represent a tab as 2 spaces
+vim.opt.shiftwidth = 2     -- Indent by 2 spaces
+vim.opt.softtabstop = 2    -- Backspace removes 2 spaces at a time
+vim.opt.expandtab = true   -- Convert tabs to spaces
+vim.opt.autoindent = true  -- Maintain indentation level on new lines
 vim.opt.smartindent = true -- Auto-indent based on syntax
 
 vim.o.list = true
