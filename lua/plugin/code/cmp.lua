c34297308016247b71cade1c24d5cadccc8b4aed
haquocdat543 <wwwdatha543@gmail.com>
2025-05-13 15:50:48 +0700

    feat(core/plugin): code/cmp - singleELementEachLine

--- lua/plugin/code/cmp.lua
+++ lua/plugin/code/cmp.lua
@@ -82,11 +82,21 @@ return {
 			}),
 
 			sources = {
-				{ name = "luasnip" },
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "nvim_lua" },
-				{ name = "path" },
+				{
+					name = "luasnip",
+				},
+				{
+					name = "nvim_lsp",
+				},
+				{
+					name = "buffer",
+				},
+				{
+					name = "nvim_lua",
+				},
+				{
+					name = "path",
+				},
 			},
 		})
 	end,
--- lua/plugin/code/linter.lua
+++ lua/plugin/code/linter.lua
@@ -4,12 +4,24 @@ return {
 		local lint = require("lint")
 		-- Define linters for specific languages
 		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			python = { "flake8" },
-			sh = { "shellcheck" },
-			lua = { "luacheck" },
-			go = { "golangci-lint" },
+			javascript = {
+				"eslint_d",
+			},
+			typescript = {
+				"eslint_d",
+			},
+			python = {
+				"flake8",
+			},
+			sh = {
+				"shellcheck",
+			},
+			lua = {
+				"luacheck",
+			},
+			go = {
+				"golangci-lint",
+			},
 		}
 	end,
 }
