7d1a8efb010fd1bb33978ac0296c4722b2c04802
haquocdat543 <wwwdatha543@gmail.com>
2025-06-09 10:57:58 +0700

    feat(core/plugin): snippet/luasnip/html - add MathJax

--- lua/plugin/snippet/luasnip/html.lua
+++ lua/plugin/snippet/luasnip/html.lua
@@ -7,6 +7,14 @@ local i = luasnip.insert_node
 
 a("html", {
 
+	-- MathJax via CDN
+	s("mathjax", {
+		t({
+			"<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>",
+			"<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"> </script>",
+		})
+	}),
+
 	-- FontAwesome via CDN
 	s("fontawesome", {
 		t({
