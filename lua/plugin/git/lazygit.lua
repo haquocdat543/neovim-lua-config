618bf30d63ece1978e3f7c34f5eb4898d4c72648
haquocdat543 <wwwdatha543@gmail.com>
2025-07-06 21:15:12 +0700

    fix(plugin/lazygit): convert to neovim script

--- lua/plugin/git/lazygit.lua
+++ lua/plugin/git/lazygit.lua
@@ -1,24 +1,14 @@
 return {
 	"kdheepak/lazygit.nvim",
 	config = function()
-		vim.cmd([[let g:lazygit_floating_window_winblend = 0 " transparency of floating window ]])
-		vim.cmd([[let g:lazygit_floating_window_scaling_factor = 0.9 " scaling factor for floating window ]])
-		vim.cmd(
-			[[let g:lazygit_floating_window_border_chars = [
-			'╭',
-			'─',
-			'╮',
-			'│',
-			'╯','─',
-			'╰',
-			'│'],
-			" customize lazygit popup window border characters
-			]]
-		)
-		vim.cmd([[let g:lazygit_floating_window_use_plenary = 0 " use plenary.nvim to manage floating window if available ]])
-		vim.cmd([[let g:lazygit_use_neovim_remote = 1 " fallback to 0 if neovim-remote is not installed ]])
-
-		vim.cmd([[let g:lazygit_use_custom_config_file_path = 0 " config file path is evaluated if this value is 1 ]])
-		vim.cmd([[let g:lazygit_config_file_path = '' " custom config file path ]])
+		vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+		vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+		vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' } -- customize lazygit popup window border characters
+		vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
+		vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+		vim.g.lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
+		vim.g.lazygit_config_file_path = '' -- custom config file path
+		vim.g.lazygit_config_file_path = {} -- table of custom config file paths
+		vim.g.lazygit_on_exit_callback = nil -- optional function callback when exiting lazygit (useful for example to refresh some UI elements after lazy git has made some changes)
 	end,
 }
