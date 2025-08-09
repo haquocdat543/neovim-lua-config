e51fa77eec6fd985197723a0f9fb90d471a852b6
haquocdat543 <wwwdatha543@gmail.com>
2025-08-09 09:37:15 +0700

    feat(core/colorscheme): use [tokyonight-day]

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,3 +1,4 @@
-vim.cmd[[colorscheme gruvbox]]
+-- vim.cmd[[colorscheme gruvbox]]
 -- vim.cmd([[colorscheme tokyonight-night]])
+vim.cmd([[colorscheme tokyonight-day]])
 -- vim.cmd [[colorscheme catppuccin]]
