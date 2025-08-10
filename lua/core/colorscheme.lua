ad715bbb3d12c184d74ce0d183cd9f9546574ba1
haquocdat543 <wwwdatha543@gmail.com>
2025-08-10 09:19:15 +0700

    feat(core/colorscheme): convert to nvim api

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,4 +1,4 @@
--- vim.cmd[[colorscheme gruvbox]]
--- vim.cmd([[colorscheme tokyonight-night]])
-vim.cmd([[colorscheme tokyonight-day]])
--- vim.cmd [[colorscheme catppuccin]]
+-- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("tokyonight-night")
+-- vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("tokyonight-day")
