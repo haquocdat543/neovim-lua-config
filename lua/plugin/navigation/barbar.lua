f047a302934b0f11628086b9ef02eed85a363c00
haquocdat543 <wwwdatha543@gmail.com>
2025-08-19 20:27:31 +0700

    feat(barbar): add config

--- lua/plugin/navigation/barbar.lua
+++ lua/plugin/navigation/barbar.lua
@@ -1,11 +1,20 @@
 return {
 	"romgrk/barbar.nvim",
 	dependencies = {
-		"lewis6991/gitsigns.nvim",    -- OPTIONAL: for git status
+		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
 		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 	},
 	init = function()
 		vim.g.barbar_auto_setup = false
+		require("barbar").setup({
+			icons = {
+				gitsigns = {
+					added = { enabled = true, icon = "+" },
+					changed = { enabled = true, icon = "~" },
+					deleted = { enabled = true, icon = "-" },
+				},
+			},
+		})
 	end,
 	opts = {
 		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
