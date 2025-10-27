45191f5a5e47fce5197e6a96367a5ba683c8d7f2
haquocdat543 <wwwdatha543@gmail.com>
2025-10-27 23:10:13 +0700

    plugin: enable tidal

--- lua/plugin/plugin.lua
+++ lua/plugin/plugin.lua
@@ -89,8 +89,7 @@ require("lazy").setup({
 	-- require("plugin.code.mcphub"),
 	require("plugin.code.avante"),
 	require("plugin.code.openscad"),
-	-- require("plugin.code.tidal-vim"),
-	-- require("plugin.code.tidal"),
+	require("plugin.code.tidal-vim"),
 	-- require("plugin.code.sonicpi"),
 	require("plugin.code.supercollider"),
 
