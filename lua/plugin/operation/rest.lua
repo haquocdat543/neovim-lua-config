3eeeba136ca4694eeb3e72630d74ca81419f7433
haquocdat543 <wwwdatha543@gmail.com>
2025-11-04 23:40:44 +0700

    operation/rest: format

--- lua/plugin/operation/rest.lua
+++ lua/plugin/operation/rest.lua
@@ -1,16 +1,16 @@
 return {
-  "rest-nvim/rest.nvim",
-  config = function()
-    require("rest-nvim").setup({
-      response = {
-        hooks = {
-          decode_url = true,
-          format = true,
-        },
-      },
-      ui = {
-        winbar = true,
-      }
-    })
-  end,
+	"rest-nvim/rest.nvim",
+	config = function()
+		require("rest-nvim").setup({
+			response = {
+				hooks = {
+					decode_url = true,
+					format = true,
+				},
+			},
+			ui = {
+				winbar = true,
+			}
+		})
+	end,
 }
