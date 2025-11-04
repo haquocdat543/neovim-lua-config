413ed7eede0ef761eaed84642639b2ee3dfe9d5c
haquocdat543 <wwwdatha543@gmail.com>
2025-11-04 22:30:54 +0700

    vim-floatterm: get global variables

--- lua/core/vim.lua
+++ lua/core/vim.lua
@@ -1,9 +1,3 @@
-vim.g.floaterm_position = "topright"
-vim.g.floaterm_width = 0.6
-vim.g.floaterm_height = 0.8
-vim.g.floaterm_title = "Terminal $1/$2"
-vim.g.floaterm_wintype = "float"
-vim.g.floaterm_rootmarkers = { ".pro" }
 vim.g.gitgutter_sign_added = "+"
 vim.g.gitgutter_sign_modified = "*"
 vim.g.gitgutter_sign_removed = "x"
--- lua/plugin/operation/floatterm.lua
+++ lua/plugin/operation/floatterm.lua
@@ -1,3 +1,11 @@
 return {
 	"voldikss/vim-floaterm",
+	config = function()
+		vim.g.floaterm_position = "topright"
+		vim.g.floaterm_width = 0.6
+		vim.g.floaterm_height = 0.8
+		vim.g.floaterm_title = "Terminal $1/$2"
+		vim.g.floaterm_wintype = "float"
+		vim.g.floaterm_rootmarkers = { ".pro" }
+	end
 }
