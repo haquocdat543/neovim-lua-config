58631d2fce1be3cadd7629f15ef89e912aa8fcff
haquocdat543 <wwwdatha543@gmail.com>
2025-11-04 21:20:33 +0700

    plugin(todo-comments): add keymap

--- lua/plugin/task/todo-comments.lua
+++ lua/plugin/task/todo-comments.lua
@@ -7,5 +7,8 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
-	}
+	},
+
+	vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>")
+
 }
