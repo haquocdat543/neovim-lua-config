79f5f04e818bb808482c82edfe3fb50b13e7ce6a
haquocdat543 <wwwdatha543@gmail.com>
2025-11-04 21:16:47 +0700

    plugin(telescope): add vimgrep arguments

--- lua/plugin/navigation/telescope.lua
+++ lua/plugin/navigation/telescope.lua
@@ -19,6 +19,20 @@ return {
 
 		telescope.setup({
 			defaults = {
+				vimgrep_arguments = {
+					'rg',
+					'--color=never',
+					'--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'--hidden',  -- include hidden files
+					'--glob',
+					'!.git/',    -- exclude .git directory explicitly
+					'!node_modules/', -- exclude nodejs node_modules
+					'!target/',  -- exclude rust build target
+				},
 				file_ignore_patterns = {
 					"%.terragrunt%-cache/",
 					"node_modules/",
