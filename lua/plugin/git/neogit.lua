09ab974fbd34e7da5210279ccd1cfccde280e9ef
haquocdat543 <wwwdatha543@gmail.com>
2025-11-05 11:25:11 +0700

    neogit/diffview: add keymaps

--- lua/plugin/git/neogit.lua
+++ lua/plugin/git/neogit.lua
@@ -2,13 +2,22 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
+		{
+			"sindrets/diffview.nvim",
+			opts = {
+				keymaps = {
+					view = {
+						{ { "n", "v" }, "q", "<Cmd>DiffviewClose<CR>", { silent = true } },
+					},
+				},
+			},
+		},
 
 		-- Only one of these is needed, not both.
 		"nvim-telescope/telescope.nvim", -- optional
-		"ibhagwan/fzf-lua",            -- optional
+		"ibhagwan/fzf-lua", -- optional
 	},
 	keys = {
 		{ "<leader>ng", "<cmd>Neogit<cr>", desc = "Open Neogit" },
-	}
+	},
 }
