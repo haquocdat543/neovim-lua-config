d81b2cb0d74a21a7c00fc593aee044dd95205566
haquocdat543 <wwwdatha543@gmail.com>
2025-12-09 21:39:54 +0700

    spectre: update config

--- lua/plugin/navigation/spectre.lua
+++ lua/plugin/navigation/spectre.lua
@@ -1,7 +1,60 @@
 return {
 	"nvim-pack/nvim-spectre",
 	config = function()
-		require("spectre").setup()
+		require("spectre").setup({
+			find_engine = {
+				-- rg is map with finder_cmd
+				["rg"] = {
+					cmd = "rg",
+					-- default args
+					args = {
+						-- "--color=always",
+						"--pcre2", -- lookbehind and lookahead
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+					},
+					options = {
+						["ignore-case"] = {
+							value = "--ignore-case",
+							icon = "[I]",
+							desc = "ignore case",
+						},
+						["hidden"] = {
+							value = "--hidden",
+							desc = "hidden file",
+							icon = "[H]",
+						},
+						-- you can put any rg search option you want here it can toggle with
+						-- show_option function
+					},
+				},
+			},
+			replace_engine = {
+				["sd"] = {
+					cmd = "sd",
+					options = {
+						["ignore-case"] = {
+							value = "-f i",
+							icon = "[I]",
+							desc = "ignore case",
+						},
+					},
+				},
+			},
+			default = {
+				find = {
+					--pick one of item in find_engine
+					cmd = "rg",
+					options = { "ignore-case" },
+				},
+				replace = {
+					--pick one of item in replace_engine
+					cmd = "sd",
+				},
+			},
+		})
 	end,
 	keys = {
 		{
