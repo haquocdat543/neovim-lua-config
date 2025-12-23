d84a2b6b6bf58f5667a889219b73e78d312ef6a8
haquocdat543 <wwwdatha543@gmail.com>
2025-12-23 20:57:37 +0700

    option: add os detect

--- lua/core/option.lua
+++ lua/core/option.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "      -- for normal mode
+vim.g.mapleader = " " -- for normal mode
 vim.g.maplocalleader = " " -- for local leader mappings
 vim.opt.title = true
 vim.opt.hlsearch = true
@@ -16,14 +16,26 @@ vim.opt.wildmenu = true
 vim.opt.laststatus = 2
 vim.opt.cursorline = true
 
-vim.opt.tabstop = 2        -- Visually represent a tab as 2 spaces
-vim.opt.shiftwidth = 2     -- Indent by 2 spaces
-vim.opt.softtabstop = 2    -- Backspace removes 2 spaces at a time
-vim.opt.expandtab = true   -- Convert tabs to spaces
-vim.opt.autoindent = true  -- Maintain indentation level on new lines
+vim.opt.tabstop = 2 -- Visually represent a tab as 2 spaces
+vim.opt.shiftwidth = 2 -- Indent by 2 spaces
+vim.opt.softtabstop = 2 -- Backspace removes 2 spaces at a time
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.autoindent = true -- Maintain indentation level on new lines
 vim.opt.smartindent = true -- Auto-indent based on syntax
 
 vim.opt.viminfo = "'100,<1000000,s100000,h"
 
 -- Set option
 vim.api.nvim_win_set_option(0, "paste", false)
+
+-- Detect OS / environment
+local uname = vim.loop.os_uname().sysname
+
+-- Adjust paths based on OS
+if uname == "Linux" then
+	vim.g.python3_host_prog = "/usr/bin/python3"
+	vim.o.clipboard = "unnamedplus" -- xclip/xsel should work
+elseif uname == "Darwin" then
+	vim.g.python3_host_prog = "/usr/local/bin/python3"
+	vim.o.clipboard = "unnamedplus" -- pbcopy/pbpaste
+end
