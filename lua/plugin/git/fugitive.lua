d6f0a34c4d955b92d00c4d08c596276890ad070a
haquocdat543 <wwwdatha543@gmail.com>
2026-01-22 13:52:17 +0000

    fugitive: turn off

--- lua/plugin/git/fugitive.lua
+++ lua/plugin/git/fugitive.lua
@@ -1,16 +1,17 @@
 return {
 	"tpope/vim-fugitive",
+	enabled = false,
 	keys = {
-		{ "<leader>gb", "<cmd>Git blame<cr>",  desc = "Open git blame terminal" },
-		{ "<leader>gs", "<cmd>G status<cr>",   desc = "Show status" },
-		{ "<leader>ga", "<cmd>G add<cr>",      desc = "Git add" },
-		{ "<leader>gA", "<cmd>G add .<cr>",    desc = "Git add all" },
-		{ "<leader>gc", "<cmd>G commit<cr>",   desc = "Git commit" },
-		{ "<leader>gb", "<cmd>G checkout<cr>", desc = "Git checkout" },
-		{ "<leader>gB", "<cmd>G checkout -b ", desc = "Git create new branch" },
-		{ "<leader>gp", "<cmd>G pull<cr>",     desc = "Git pull" },
-		{ "<leader>gP", "<cmd>G push<cr>",     desc = "Git push" },
-		{ "<leader>gl", "<cmd>G log<cr>",      desc = "Git log" },
-		{ "<leader>gr", "<cmd>G reflog<cr>",   desc = "Git reflog" },
+		-- { "<leader>gb", "<cmd>Git blame<cr>",  desc = "Open git blame terminal" },
+		-- { "<leader>gs", "<cmd>G status<cr>",   desc = "Show status" },
+		-- { "<leader>ga", "<cmd>G add<cr>",      desc = "Git add" },
+		-- { "<leader>gA", "<cmd>G add .<cr>",    desc = "Git add all" },
+		-- { "<leader>gc", "<cmd>G commit<cr>",   desc = "Git commit" },
+		-- { "<leader>gb", "<cmd>G checkout<cr>", desc = "Git checkout" },
+		-- { "<leader>gB", "<cmd>G checkout -b ", desc = "Git create new branch" },
+		-- { "<leader>gp", "<cmd>G pull<cr>",     desc = "Git pull" },
+		-- { "<leader>gP", "<cmd>G push<cr>",     desc = "Git push" },
+		-- { "<leader>gl", "<cmd>G log<cr>",      desc = "Git log" },
+		-- { "<leader>gr", "<cmd>G reflog<cr>",   desc = "Git reflog" },
 	}
 }
