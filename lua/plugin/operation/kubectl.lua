33bb1b83f42e6f02450b099d943bfd1e37263f79
haquocdat543 <wwwdatha543@gmail.com>
2026-01-22 14:13:46 +0000

    kubectl: turn off

--- lua/plugin/operation/kubectl.lua
+++ lua/plugin/operation/kubectl.lua
@@ -1,9 +1,7 @@
 return {
 	"ramilito/kubectl.nvim",
+	enabled = false,
 	keys = {
-		{
-			"<leader>kk",
-			'<cmd>lua require("kubectl").toggle()<CR>',
-		},
+		-- { "<leader>kk", '<cmd>lua require("kubectl").toggle()<CR>', },
 	},
 }
