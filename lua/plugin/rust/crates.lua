9ec010b1e72dbd62944a75f93e3f7564ecd79b92
haquocdat543 <wwwdatha543@gmail.com>
2026-02-04 11:45:09 +0700

    rust(crate): add key maps

--- lua/plugin/rust/crates.lua
+++ lua/plugin/rust/crates.lua
@@ -2,6 +2,12 @@ return {
 	"saecki/crates.nvim",
 	tag = "stable",
 	event = { "BufRead Cargo.toml" },
+	keys = {
+		{ "<leader>r1", "<cmd>Crates show_features_popup<cr>", desc = "Open rust features popup" },
+		{ "<leader>r2", "<cmd>Crates show_versions_popup<cr>", desc = "Open rust versions popup" },
+		{ "<leader>r3", "<cmd>Crates show_dependencies_popup<cr>", desc = "Open rust dependencies popup" },
+		{ "<leader>r4", "<cmd>Crates show_crate_popup<cr>", desc = "Open rust crate popup" },
+	},
 	config = function()
 		require("crates").setup()
 	end,
