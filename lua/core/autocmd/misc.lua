ece689d500ac02625845705a6f28f24b98dc627d
haquocdat543 <wwwdatha543@gmail.com>
2026-02-13 02:30:53 +0000

    autocmd: add LspStop on .env

--- lua/core/autocmd/misc.lua
+++ lua/core/autocmd/misc.lua
@@ -16,3 +16,22 @@ vim.api.nvim_create_autocmd(
 		end,
 	}
 )
+
+-- ENV
+vim.api.nvim_create_autocmd(
+	{
+		"BufRead",
+		"BufReadPost",
+		"BufNewFile",
+		"BufWinEnter",
+		"BufEnter",
+		"BufWritePre",
+		"BufWritePost",
+	},
+	{
+		pattern = ".env",
+		callback = function()
+			vim.cmd("LspStop")
+		end,
+	}
+)
