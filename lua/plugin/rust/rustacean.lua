ec31f6b25ff607cb7859abfe72a22261748471bc
haquocdat543 <wwwdatha543@gmail.com>
2026-02-18 02:13:31 +0000

    rustacean: upate

--- lua/plugin/rust/rustacean.lua
+++ lua/plugin/rust/rustacean.lua
@@ -1,6 +1,35 @@
 return {
 	"mrcjkb/rustaceanvim",
 	enabled = true,
-	version = "^6", -- Recommended
+	version = "^8", -- Recommended
 	lazy = false,  -- This plugin is already lazy
+	config = function()
+		vim.g.rustaceanvim = {
+			-- Plugin configuration
+			tools = {
+			},
+			-- LSP configuration
+			server = {
+				on_attach = function(client, bufnr)
+					-- you can also put keymaps in here
+				end,
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						procMacro = {
+							enable = true,
+						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+						},
+					},
+				},
+			},
+			-- DAP configuration
+			dap = {
+			},
+		}
+	end
 }
