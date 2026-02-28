820e56e6377d2d0b3af9016019842c3ed3c282c2
haquocdat543 <wwwdatha543@gmail.com>
2026-02-28 11:41:48 +0700

    ranger: add ui

--- lua/plugin/navigation/ranger.lua
+++ lua/plugin/navigation/ranger.lua
@@ -1,7 +1,16 @@
 return {
 	"kelly-lin/ranger.nvim",
 	config = function()
-		require("ranger-nvim").setup({ replace_netrw = true })
+		require("ranger-nvim").setup({
+			replace_netrw = true,
+			ui = {
+				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+				height = 0.9,
+				width = 0.9,
+				x = 0.5,
+				y = 0.5,
+			},
+		})
 		vim.api.nvim_set_keymap("n", "<leader>q", "", {
 			noremap = true,
 			callback = function()
