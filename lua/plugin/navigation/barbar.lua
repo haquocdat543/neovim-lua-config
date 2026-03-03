965b6f39d209a19458826adfc15230696f9c5aaf
haquocdat543 <wwwdatha543@gmail.com>
2026-03-03 01:04:49 +0000

    navigation(barbar): set tag

--- lua/plugin/navigation/barbar.lua
+++ lua/plugin/navigation/barbar.lua
@@ -86,5 +86,5 @@ return {
 		-- insert_at_start = true,
 		-- …etc.
 	},
-	version = "^1.0.0", -- optional: only update when a new 1.x version is released
+	tag = "1.9.1", -- optional: only update when a new 1.x version is released
 }
