f7547c237651e281a36eba461d97e8b990a0e155
haquocdat543 <wwwdatha543@gmail.com>
2026-03-10 02:34:32 +0000

    ident: update

--- lua/plugin/visualize/indent.lua
+++ lua/plugin/visualize/indent.lua
@@ -1,3 +1,21 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	config = function()
+		local highlight = {
+			-- "CursorColumn",
+			"Whitespace",
+		}
+		require("ibl").setup {
+			indent = { highlight = highlight, char = "│" },
+			whitespace = {
+				highlight = highlight,
+				remove_blankline_trail = false,
+			},
+			scope = { enabled = true },
+		}
+	end,
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
 }
