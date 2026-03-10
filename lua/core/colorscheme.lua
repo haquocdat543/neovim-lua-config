1731e2c7f0ef842aa4b595c5f63f4b69b90dab9f
haquocdat543 <wwwdatha543@gmail.com>
2026-03-10 12:26:17 +0000

    colorscheme: set gruvbox

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -2,9 +2,11 @@
 -- vim.cmd.colorscheme("tokyonight-night")
 -- vim.cmd.colorscheme("catppuccin-mocha")
 -- vim.cmd.colorscheme("tokyonight-day")
-local time = os.date("*t")
-if time.hour < 5 or time.hour > 17 then
-	vim.cmd([[colorscheme tokyonight-day]])
-else
-	vim.cmd([[colorscheme tokyonight-night]])
-end
+-- local time = os.date("*t")
+-- if time.hour < 5 or time.hour > 17 then
+-- 	vim.cmd([[colorscheme tokyonight-day]])
+-- else
+-- 	vim.cmd([[colorscheme tokyonight-night]])
+-- end
+
+vim.cmd([[colorscheme gruvbox]])
