f2a4b2d4a9b5dc272414574ff0577da27951de1c
haquocdat543 <wwwdatha543@gmail.com>
2026-08-24 18:12:30 +0700

    fix(rest|crates): turn on when read specific files

--- lua/plugin/operation/rest.lua
+++ lua/plugin/operation/rest.lua
@@ -1,6 +1,7 @@
 return {
 	"rest-nvim/rest.nvim",
 	enabled = true,
+	ft = { "http" },
 	config = function()
 		require("rest-nvim").setup({
 			response = {
@@ -13,17 +14,18 @@ return {
 				winbar = true,
 			},
 		})
-		vim.api.nvim_create_autocmd(
-			{
-				"BufRead",
-			},
-			{
-				pattern = "*.http",
-				callback = function()
-					vim.keymap.set("n", "<leader>rr", "<CMD>Rest run<CR>", { desc = "Run RestfulAPI under cursor" })
-					vim.keymap.set("n", "<leader>rc", "<C-w>h<C-w>c", { desc = "Close left window" })
-				end,
-			}
-		)
+		vim.api.nvim_create_autocmd({
+			"BufRead",
+		}, {
+			pattern = "*.http",
+			callback = function()
+				vim.keymap.set("n", "<leader>rr", "<CMD>Rest run<CR>", {
+					desc = "Run RestfulAPI under cursor",
+				})
+				vim.keymap.set("n", "<leader>rc", "<C-w>h<C-w>c", {
+					desc = "Close left window",
+				})
+			end,
+		})
 	end,
 }
--- lua/plugin/rust/crates.lua
+++ lua/plugin/rust/crates.lua
@@ -1,22 +1,42 @@
 return {
 	"saecki/crates.nvim",
 	tag = "stable",
+	event = {
+		"BufRead Cargo.toml",
+		"BufNewFile Cargo.toml",
+	},
 	config = function()
 		require("crates").setup()
-		vim.api.nvim_create_autocmd(
-			{
-				"BufRead",
-			},
-			{
-				pattern = "Cargo.toml",
-				callback = function()
-					vim.keymap.set("n", "<leader>r1", "<CMD>Crates show_features_popup<CR>", { desc = "Open rust features popup" })
-					vim.keymap.set("n", "<leader>r2", "<CMD>Crates show_versions_popup<CR>", { desc = "Open rust versions popup" })
-					vim.keymap.set("n", "<leader>r3", "<CMD>Crates show_crate_popup<CR>", { desc = "Open rust crate popup" })
-					vim.keymap.set("n", "<leader>r4", "<CMD>Crates show_dependencies_popup<CR>",
-						{ desc = "Open rust dependencies popup" })
-				end,
-			}
-		)
+		vim.api.nvim_create_autocmd({
+			"BufRead",
+		}, {
+			pattern = "Cargo.toml",
+			callback = function()
+				vim.keymap.set(
+					"n",
+					"<leader>r1",
+					"<CMD>Crates show_features_popup<CR>",
+					{ desc = "Open rust features popup" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>r2",
+					"<CMD>Crates show_versions_popup<CR>",
+					{ desc = "Open rust versions popup" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>r3",
+					"<CMD>Crates show_crate_popup<CR>",
+					{ desc = "Open rust crate popup" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>r4",
+					"<CMD>Crates show_dependencies_popup<CR>",
+					{ desc = "Open rust dependencies popup" }
+				)
+			end,
+		})
 	end,
 }
